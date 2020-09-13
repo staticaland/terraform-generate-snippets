e4151fe7262cf67aa38cc788921f48b78f38cee7
Anders Pettersen <anders.k.pettersen@protonmail.com>
2020-09-13 19:03:28 +0200

    Rudimentary commit

---
+++ tgs.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import subprocess
+import io
+
+
+for item in os.scandir("providers"):
+
+    out_bytes = False
+
+    try:
+        out_bytes = subprocess.check_output(
+            ["terraform", "providers", "schema", "-json"],
+            cwd=item.path,
+            stderr=subprocess.STDOUT,
+        )
+    except subprocess.CalledProcessError as e:
+        if "no suitable version installed" in e.output.decode("utf-8"):
+            subprocess.run(
+                ["terraform", "init"], cwd=item.path, stderr=subprocess.STDOUT
+            )
+            out_bytes = subprocess.check_output(
+                ["terraform", "providers", "schema", "-json"],
+                cwd=item.path,
+                stderr=subprocess.STDOUT,
+            )
+
+    if out_bytes:
+
+        f = io.BytesIO(out_bytes)
+        data = json.load(f)
+
+        resource_schemas = data.get("provider_schemas").get(item.name).get("resource_schemas")
+
+        for resource_name, resource_schema in resource_schemas.items():
+
+            print(f'resource "{resource_name}" "{{$1:name}}" {{')
+            attributes = resource_schema.get("block").get("attributes")
+
+            for pos, attribute in enumerate(attributes, start=1):
+                print(f'    {attribute} = "{{${pos}:{attribute}}}"')
+
+            print("}")
+
+        data_source_schemas = data.get("provider_schemas").get(item.name).get("data_source_schemas")
+
+        for data_source_name, data_source_schema in data_source_schemas.items():
+
+            print(f'data "{data_source_name}" "{{$1:name}}" {{')
+            attributes = data_source_schema.get("block").get("attributes")
+
+            for pos, attribute in enumerate(attributes, start=1):
+                print(f'    {attribute} = "{{${pos}:{attribute}}}"')
+
+            print("}")
