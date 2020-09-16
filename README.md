fc655f3cbc007646444de2afba24f8401e09362a
Anders Pettersen <anders.k.pettersen@protonmail.com>
2020-09-16 20:34:57 +0200

    Use jinja2 for snippet templates

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-Generate editor snippets based on Terraform provider schemas.
+Generate editor snippets based on Terraform provider schemas (such as
+[AWS](https://github.com/terraform-providers/terraform-provider-aws/tree/master/aws)).
 
 Parses [`terraform providers schema
 -json`](https://www.terraform.io/docs/commands/providers/schema.html) and
 creates snippet files based on the JSON.
 
 - [x] YASnippet support
-- [ ] Support templates for multiple engines
+- [x] Support templates for multiple engines
+- [ ] Only keep required attributes
 
 Currently only supporting AWS and Azure for
 [YASnippet](https://github.com/joaotavora/yasnippet), but should be fairly easy
--- tgs.py
+++ tgs.py
@@ -5,24 +5,20 @@ import os
 import subprocess
 import io
 
+import jinja2
 
-def make_data_source_snippet(data_source_schemas):
+JINJA_TEMPLATE = """
+# -*- mode: snippet -*-
+# name: r_[[name]]
+# key: r_[[name]]
+# --
 
-    for data_source_name, data_source_schema in data_source_schemas.items():
-
-        yield ("# -*- mode: snippet -*-")
-        yield (f"# name: r_{data_source_name}")
-        yield (f"# key: r_{data_source_name}")
-        yield ("# --")
-
-        yield (f'data "{data_source_name}" "{{$1:name}}" {{')
-
-        attributes = data_source_schema.get("block").get("attributes")
-
-        for pos, attribute in enumerate(attributes, start=2):
-            yield (f'    {attribute} = "{{${pos}:{attribute}}}"')
-
-        yield ("}")
+[[thing]] "[[name]]" "${1:this}" {
+[% for pos, a in enumerated_attributes %]
+    [[a]] = "${[[pos]]:[[a]]}"
+[% endfor %]
+}
+"""
 
 
 def get_terraform_schema(provider_path):
@@ -41,7 +37,7 @@ def get_terraform_schema(provider_path):
 
     except subprocess.CalledProcessError as e:
 
-        if "no suitable version installed" in e.output.decode("utf-8"):
+        if "Could not load plugin" in e.output.decode("utf-8"):
 
             subprocess.run(
                 ["terraform", "init"], cwd=provider_path, stderr=subprocess.STDOUT
@@ -55,22 +51,14 @@ def get_terraform_schema(provider_path):
 
             return out_bytes
 
-
-def write_snippet_to_path(snippet):
-
-    snippet_type = "hello"
-    snippet_name = "hello"
-
-    with open(f"snippets/terraform-mode/{snippet_type}_{snippet_name}", "w") as f:
-        f.write("\n".join(snippet))
+        raise
 
 
 def main():
 
     for dir_entry in os.scandir("providers"):
 
-        print(dir_entry.path)
-
+        print(f"About to run `terraform providers schema -json` in {dir_entry.path}")
         json_schema_bytes = get_terraform_schema(dir_entry.path)
 
         if json_schema_bytes:
@@ -78,38 +66,57 @@ def main():
             f = io.BytesIO(json_schema_bytes)
             provider_schemas = json.load(f).get("provider_schemas")
 
-            resource_schemas = (
-                provider_schemas.get(dir_entry.name).get("resource_schemas")
+            # Get the first dict key
+            schemas = next(iter(provider_schemas.values()))
+
+            resource_schemas = schemas.get("resource_schemas")
+
+            data_source_schemas = schemas.get("data_source_schemas")
+
+            environment = jinja2.Environment(
+                loader=jinja2.BaseLoader(),
+                block_start_string="[%",
+                block_end_string="%]",
+                variable_start_string="[[",
+                variable_end_string="]]",
+                autoescape=False,
+                trim_blocks=True,
             )
+
+            template = environment.from_string(JINJA_TEMPLATE)
+
+            print(f"Loaded schema JSON for {dir_entry.name} - templating and writing snippet files")
 
             for resource_name, resource_schema in resource_schemas.items():
 
-                snippet = []
+                snippet = template.render(
+                    thing="resource",
+                    name=resource_name,
+                    enumerated_attributes=enumerate(
+                        resource_schema.get("block").get("attributes"), start=2
+                    ),
+                )
 
-                snippet.append("# -*- mode: snippet -*-")
-                snippet.append(f"# name: r_{resource_name}")
-                snippet.append(f"# key: r_{resource_name}")
-                snippet.append("# --")
+                with open(
+                    f"snippets/terraform-mode/r_{resource_name}", "w"
+                ) as f:
+                    f.write(snippet)
 
-                snippet.append(f'resource "{resource_name}" "${{1:name}}" {{')
+            for data_source_name, data_source_schema in data_source_schemas.items():
 
+                snippet = template.render(
+                    thing="data",
+                    name=data_source_name,
+                    enumerated_attributes=enumerate(
+                        data_source_schema.get("block").get("attributes"), start=2
+                    ),
+                )
 
-                attributes = resource_schema.get("block").get("attributes")
-
-                for pos, attribute in enumerate(attributes, start=2):
-                    if attributes.get(attribute).get("required") == True:
-                        snippet.append(f'    {attribute} = "${{{pos}:{attribute}}}"')
-
-                snippet.append("}")
-
-                with open(f"snippets/terraform-mode/r_{resource_name}", "w") as f:
-                    f.write("\n".join(snippet))
-
-            data_source_schemas = (
-                provider_schemas.get(dir_entry.name).get("data_source_schemas")
-            )
-
-            print("\n".join(make_data_source_snippet(data_source_schemas)))
+                with open(
+                    f"snippets/terraform-mode/d_{data_source_name}",
+                    "w",
+                ) as f:
+                    f.write(snippet)
 
 
 if __name__ == "__main__":
